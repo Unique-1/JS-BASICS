4b6cc8096a4a97d756a1db51b1ffde645cc20bc4
Olarewaju Kola Abbey <codetechdev001@gmail.com>
2024-03-19 17:21:47 +0100

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # JS-BASICS
 javascript tutorial for beginners with some vscoed trick
+
+# install this extension
+    "live server" from vs code extension in other to run ur codedirectly from vscode
+# how to generate html boilerplate
+    click on "!" + "tab key"
+# to run ur code from vscode
+    look at the down part of ur vscode and click on "Go live"
+# how to run js file in vscode terminal
+    node index.js
---
+++ index.js
@@ -0,0 +1,22 @@
+// console.log('hello world');
+
+// keyword let is used to declared variable in js
+
+// variable
+// a variable cannot be researved keywork, it should be meaningful, 
+// cannot start a number like (1name), cannot conatin a space or hyphen(-)
+// they are case sensitive 
+let name = 'codetech';
+console.log(name);
+
+// constant 
+
+// keyword const is used to declare constant 
+const interestRate = 0.3;
+try{
+    interestRate = 99;
+}catch(err){
+    console.log(err)
+}
+
+console.log(interestRate);
